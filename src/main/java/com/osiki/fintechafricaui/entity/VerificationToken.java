843f3f71fab83c132c92e92a9d52f6b47bebc947
Segun Osiki <osiki.segun@gmail.com>
2023-03-11 21:34:28 +0100

    create verification token for user

--- src/main/java/com/osiki/fintechafricaui/entity/VerificationToken.java
+++ src/main/java/com/osiki/fintechafricaui/entity/VerificationToken.java
@@ -1,19 +1,57 @@
 package com.osiki.fintechafricaui.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.Date;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class VerificationToken {
 
+    private static final int EXPIRATION_TIME = 10;
+
     @Id
+
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private long id;
+
     private String token;
-    private Data expirationTime;
+
+    private Date expirationTime;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id",
+    nullable = false,
+    foreignKey = @ForeignKey(name = "FK_USER_VERIFY_TOKEN"))
+    private Users user;
+
+    public VerificationToken(Users user, String token){
+        super();
+        this.user = user;
+        this.token = token;
+        this.expirationTime = calculateExpirationDate(EXPIRATION_TIME);
+
+    }
+
+    public VerificationToken(String token) {
+        super();
+        this.token = token;
+        this.expirationTime = calculateExpirationDate(EXPIRATION_TIME);
+
+    }
+
+    private Date calculateExpirationDate(int expirationTime) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(new Date().getTime());
+        calendar.add(Calendar.MINUTE, expirationTime);
+
+        return new Date(calendar.getTime().getTime());
+    }
+
+
 }
