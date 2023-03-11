ac879531e75a9a107c3218c159bf8b3358a85a72
Segun Osiki <osiki.segun@gmail.com>
2023-03-11 12:54:32 +0100

    first commit

---
+++ src/main/java/com/osiki/fintechafricaui/FintechAfricaUiApplication.java
@@ -0,0 +1,13 @@
+package com.osiki.fintechafricaui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechAfricaUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechAfricaUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osiki/fintechafricaui/FintechAfricaUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiki.fintechafricaui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechAfricaUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
