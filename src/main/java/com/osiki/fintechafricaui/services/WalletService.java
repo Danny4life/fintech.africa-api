28d76e64b0ad9192f92185ae6f0aa3e73bfb042a
Segun Osiki <osiki.segun@gmail.com>
2023-03-21 10:06:34 +0100

    implement view wallet details logic

--- src/main/java/com/osiki/fintechafricaui/services/WalletService.java
+++ src/main/java/com/osiki/fintechafricaui/services/WalletService.java
@@ -1,7 +1,8 @@
 package com.osiki.fintechafricaui.services;
 
+import com.osiki.fintechafricaui.exception.UserNotFoundException;
 import com.osiki.fintechafricaui.model.WalletModel;
 
 public interface WalletService {
-    WalletModel viewWalletDetails();
+    WalletModel viewWalletDetails() throws UserNotFoundException;
 }
--- src/main/java/com/osiki/fintechafricaui/services/serviceImpl/WalletServiceImpl.java
+++ src/main/java/com/osiki/fintechafricaui/services/serviceImpl/WalletServiceImpl.java
@@ -1,10 +1,16 @@
 package com.osiki.fintechafricaui.services.serviceImpl;
 
+import com.osiki.fintechafricaui.entity.Users;
+import com.osiki.fintechafricaui.entity.Wallet;
+import com.osiki.fintechafricaui.exception.UserNotFoundException;
 import com.osiki.fintechafricaui.model.WalletModel;
 import com.osiki.fintechafricaui.respository.UsersRepository;
 import com.osiki.fintechafricaui.respository.WalletRepository;
 import com.osiki.fintechafricaui.services.WalletService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,8 +23,18 @@ public class WalletServiceImpl implements WalletService {
     private UsersRepository usersRepository;
 
     @Override
-    public WalletModel viewWalletDetails() {
+    public WalletModel viewWalletDetails() throws UserNotFoundException {
         WalletModel walletModel = new WalletModel();
-        return null;
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Users users = usersRepository.findByEmail(user.getUsername());
+
+
+        Wallet wallet = users.getWallet();
+        walletModel.setBalance(walletModel.getBalance());
+        walletModel.setBankName(wallet.getBankName());
+        walletModel.setAccountNumber(wallet.getAccountNumber());
+        BeanUtils.copyProperties(users, walletModel);
+
+        return walletModel;
     }
 }
