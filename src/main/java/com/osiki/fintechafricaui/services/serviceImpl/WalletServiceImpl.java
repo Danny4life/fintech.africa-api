9e2173e07826ae25b0ac4b1c38be5e455c905b6b
Segun Osiki <osiki.segun@gmail.com>
2023-03-21 08:56:12 +0100

    inject wallet and users repository dependecies

--- src/main/java/com/osiki/fintechafricaui/services/serviceImpl/WalletServiceImpl.java
+++ src/main/java/com/osiki/fintechafricaui/services/serviceImpl/WalletServiceImpl.java
@@ -1,8 +1,17 @@
 package com.osiki.fintechafricaui.services.serviceImpl;
 
+import com.osiki.fintechafricaui.respository.UsersRepository;
+import com.osiki.fintechafricaui.respository.WalletRepository;
 import com.osiki.fintechafricaui.services.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WalletServiceImpl implements WalletService {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+    @Autowired
+    private UsersRepository usersRepository;
 }
