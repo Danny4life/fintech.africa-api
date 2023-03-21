8b2edb2281c1b3cf74a979e544e881696586f8ae
Segun Osiki <osiki.segun@gmail.com>
2023-03-21 08:52:41 +0100

    create wallet service class

---
+++ src/main/java/com/osiki/fintechafricaui/respository/WalletRepository.java
@@ -0,0 +1,9 @@
+package com.osiki.fintechafricaui.respository;
+
+import com.osiki.fintechafricaui.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+}
---
+++ src/main/java/com/osiki/fintechafricaui/services/serviceImpl/WalletServiceImpl.java
@@ -0,0 +1,8 @@
+package com.osiki.fintechafricaui.services.serviceImpl;
+
+import com.osiki.fintechafricaui.services.WalletService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletServiceImpl implements WalletService {
+}
